638d7ebc81b47becb7ba338cddfc37fd981113db
nsc5171 <nsaichandra55@gmail.com>
2018-02-17 00:10:43 +0530

    added spring data jpa starter and application.yml, set server.port to 9999 added test helloworld mapping

--- src/main/java/com/nsc5171/myprojects/controller/RestSimulatorController.java
+++ src/main/java/com/nsc5171/myprojects/controller/RestSimulatorController.java
@@ -1,6 +1,15 @@
 package com.nsc5171.myprojects.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("simulator")
 public class RestSimulatorController {
+
+    @RequestMapping("/")
+    public String HelloWorld(){
+        return "helloooo";
+    }
 
 }
--- src/main/java/com/nsc5171/myprojects/launcher/LaunchSimulator.java
+++ src/main/java/com/nsc5171/myprojects/launcher/LaunchSimulator.java
@@ -1,4 +1,21 @@
 package com.nsc5171.myprojects.launcher;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.Arrays;
+
+@SpringBootApplication
 public class LaunchSimulator {
+
+    public static void main(String[] a){
+        ApplicationContext context=SpringApplication.run(LaunchSimulator.class);
+        String[] beans=context.getBeanDefinitionNames();
+        Arrays.sort(beans);
+        for(String s:beans){
+            System.out.println(s);
+        }
+    }
 }
