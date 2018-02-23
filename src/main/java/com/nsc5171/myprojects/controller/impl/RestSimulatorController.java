2826f5ca8ed8f0e4995cc74ed603d7885a8605cc
nsc5171 <nsaichandra55@gmail.com>
2018-02-23 22:50:41 +0530

    added support for GET type simulation

--- src/main/java/com/nsc5171/myprojects/controller/impl/RestSimulatorController.java
+++ src/main/java/com/nsc5171/myprojects/controller/impl/RestSimulatorController.java
@@ -8,6 +8,7 @@ import com.nsc5171.myprojects.exception.NoSuchSimulationException;
 import com.nsc5171.myprojects.service.RequestParserService;
 import lombok.extern.log4j.Log4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,14 +26,36 @@ public class RestSimulatorController extends AppController {
 
     @PostMapping(value = {"{simulator}/{keySearchLocation}/{keyPath}/{delay}",
             "{simulator}/{keySearchLocation}/{keyPath:.+}"})
-    public String getSimulation(HttpServletRequest request,
+    public String getPOSTSimulation(HttpServletRequest request,
                                 HttpServletResponse response,
                                 @PathVariable String simulator,
                                 @PathVariable String keySearchLocation,
                                 @PathVariable String keyPath,
                                 @PathVariable(required = false) Long delay)
             throws InterruptedException, IdentifierResolutionException, NoSuchSimulationException {
-        log.debug("Request for simulated response received");
+        log.debug("Request for POST simulated response received");
+        if (delay != null && delay > 0) {
+            Thread.sleep(delay);
+        }
+        String identifier = requestParserService.getIdentifierFromRequest(request, keySearchLocation, keyPath);
+        Simulation simulation = simulationRepository.findOne(new SimulationId(simulator, identifier));
+        if (simulation == null) {
+            throw new NoSuchSimulationException("Could not find a simulation mapped to simulator: " + simulator + " and identifier: " + identifier);
+        }
+//        response.setContentType(simulation.getResponseFormat());
+        return simulation.getResponse();
+
+    }
+
+    @GetMapping(value = {"{simulator}/{keySearchLocation}/{keyPath}/{delay}",
+            "{simulator}/{keySearchLocation}/{keyPath:.+}"})
+    public String getGETSimulation(HttpServletRequest request,
+                                   HttpServletResponse response,
+                                   @PathVariable String simulator,
+                                   @PathVariable String keySearchLocation,
+                                   @PathVariable String keyPath,
+                                   @PathVariable(required = false) Long delay) throws InterruptedException, IdentifierResolutionException, NoSuchSimulationException {
+        log.debug("Request for GET simulated response received");
         if (delay != null && delay > 0) {
             Thread.sleep(delay);
         }
