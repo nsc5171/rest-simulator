88502693ece7de03c0e94208d3abce5df062fc18
nsc5171 <nsaichandra55@gmail.com>
2018-02-24 15:49:14 +0530

    added created and last modified timestamps

--- src/main/java/com/nsc5171/myprojects/dao/entities/Simulation.java
+++ src/main/java/com/nsc5171/myprojects/dao/entities/Simulation.java
@@ -1,14 +1,19 @@
 package com.nsc5171.myprojects.dao.entities;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.nsc5171.myprojects.dao.entities.id.SimulationId;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
@@ -28,5 +33,14 @@ public class Simulation {
 
     @OneToMany(cascade = CascadeType.ALL,fetch = FetchType.EAGER,orphanRemoval = true)
     private List<Header> headers;
+
+    @Column(updatable = false)
+    @JsonFormat(timezone = "UTC",shape = JsonFormat.Shape.STRING,pattern = "dd-MMM-yyyy HH:mm:ss")
+    @CreationTimestamp
+    private Date created;
+
+    @JsonFormat(timezone = "UTC",shape = JsonFormat.Shape.STRING,pattern = "dd-MMM-yyyy HH:mm:ss")
+    @UpdateTimestamp
+    private Date lastModified;
 }
 
--- src/main/java/com/nsc5171/myprojects/dao/entities/projections/SimulationWithoutResponse.java
+++ src/main/java/com/nsc5171/myprojects/dao/entities/projections/SimulationWithoutResponse.java
@@ -1,10 +1,22 @@
 package com.nsc5171.myprojects.dao.entities.projections;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.nsc5171.myprojects.dao.entities.id.SimulationId;
 import lombok.Value;
+import sun.util.resources.cldr.en.TimeZoneNames_en_IN;
+
+import java.util.Date;
+import java.util.TimeZone;
 
 @Value
 public class SimulationWithoutResponse {
 
     private SimulationId simulationId;
+
+    @JsonFormat(timezone = "UTC",shape = JsonFormat.Shape.STRING,pattern = "dd-MMM-yyyy HH:mm:ss")
+    private Date created;
+
+    @JsonFormat(timezone = "UTC",shape = JsonFormat.Shape.STRING,pattern = "dd-MMM-yyyy HH:mm:ss")
+    private Date lastModified;
+
 }
