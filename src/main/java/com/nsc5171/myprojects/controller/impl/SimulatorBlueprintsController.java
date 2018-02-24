fefde9a6f0c9659ee98ac84cf0a24ff40abe7d2e
nsc5171 <nsaichandra55@gmail.com>
2018-02-24 20:29:22 +0530

    added placeholder controller class for creation of blueprint based simulations: adding TODO

--- src/main/java/com/nsc5171/myprojects/controller/impl/SimulatorBlueprintsController.java
+++ src/main/java/com/nsc5171/myprojects/controller/impl/SimulatorBlueprintsController.java
@@ -4,4 +4,5 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SimulatorBlueprintsController {
+    //TODO: Add mappings for creation of simulations through blueprints
 }
